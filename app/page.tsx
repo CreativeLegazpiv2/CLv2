ad69f183d5e8fa0ac2f356046cb6227f83825f19
inju7 <naira.edrolla@gmail.com>
2024-09-15 02:34:47 +0800

    Routed to register-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-//import Image from "next/image";
-// import { supabase } from '../lib/supabaseClient';
+// pages/index.tsx
 
-export default function Home() {
-  // console.log(supabase); 
+import React from 'react';
+import Link from 'next/link';
+
+const LandingPage = () => {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Creatives Legazpi!</h1>
+      <h2 className="text-4xl font-bold mb-8">Sign Up Here</h2>
+      <Link href="/register-form">
+        <button className="px-6 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+          Sign Up
+        </button>
+      </Link>
     </div>
   );
-}
+};
 
-// async function fetchData() {
-//   const { data, error } = await supabase.from('user_profiles').select('*');
-//   console.log({ data, error });
-// }
-
-// fetchData();
+export default LandingPage;

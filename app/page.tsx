d9066893899c3a2062ab6ee4b5f89052827213cb
inju7 <naira.edrolla@gmail.com>
2024-09-15 19:43:45 +0800

    Sign Up Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,45 @@
 
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import { NextPage } from 'next';
 
-const LandingPage = () => {
+const LandingPage: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Creatives Legazpi!</h1>
-      <h2 className="text-4xl font-bold mb-8">Sign Up Here</h2>
-      <Link href="/register-form">
-        <button className="px-6 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-          Sign Up
-        </button>
-      </Link>
+    <div className="relative h-screen overflow-hidden">
+      {/* Fixed background */}
+      <div 
+        className="fixed inset-0 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url('/assets/images/Clip%20path%20group.svg')` }}
+      />
+      
+      {/* Adjustable container for overlay and button */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="relative">
+         <div className="relative w-[1100px] h-[700px]"> {/* Explicit size here */}
+            <Image
+              src="/assets/images/image%201.svg"
+              alt="Welcome to Creative Legazpi"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          
+          {/* Image Button container */}
+          <div className="absolute inset-x-0 bottom-12 flex justify-center pb-8">
+            <Link href="/register-form">
+            <div className="inline-block w-[1000px] h-[70px] relative hover:opacity-80 transition duration-300">
+                <Image
+                  src="/assets/images/image.svg"
+                  alt="REGISTER HERE"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
